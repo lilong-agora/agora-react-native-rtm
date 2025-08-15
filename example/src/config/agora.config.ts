23f9173612a94889f791f0e784e9eae9945bab62
niki <869194277@qq.com>
2025-08-15 07:38:09 +0000

    [feat] use env values

--- example/src/config/agora.config.ts
+++ example/src/config/agora.config.ts
@@ -13,15 +13,15 @@ try {
 }
 
 // Helper function to get a value from environment variables or use default
-const getEnvValue = (key: string, defaultValue: string = '') => {
+const getEnvValue = (key: string, envKey: string, defaultValue: string = '') => {
   // Try to get from process.env (these should be set at build time in CI/CD)
   if (typeof process !== 'undefined' && process.env && process.env[key]) {
     return process.env[key] || '';
   }
   
-  // Try to get from local env file
-  if (env && env[key] !== undefined) {
-    return env[key] || '';
+  // Try to get from local env file with the original key name
+  if (env && env[envKey] !== undefined) {
+    return env[envKey] || '';
   }
   
   // Return default value
@@ -30,28 +30,28 @@ const getEnvValue = (key: string, defaultValue: string = '') => {
 
 const config = {
   // Get your own App ID at https://dashboard.agora.io/
-  appId: getEnvValue('AGORA_APP_ID', ''),
+  appId: getEnvValue('AGORA_APP_ID', 'appId', ''),
   // Certificate instead of token
-  certificate: getEnvValue('AGORA_CERTIFICATE', ''),
+  certificate: getEnvValue('AGORA_CERTIFICATE', 'certificate', ''),
   // Token is now generated, not manually entered
-  token: getEnvValue('AGORA_TOKEN', ''),
-  channelName: getEnvValue('AGORA_CHANNEL_NAME', 'rtmtestrn'),
-  uid: getEnvValue('AGORA_UID', ''),
-  logFilePath: getEnvValue('AGORA_LOG_FILE_PATH', ''),
-  server: getEnvValue('AGORA_SERVER', ''),
-  port: parseInt(getEnvValue('AGORA_PORT', '0'), 10) || 0,
-  proxyType: parseInt(getEnvValue('AGORA_PROXY_TYPE', RtmProxyType.none.toString()), 10) || RtmProxyType.none,
-  account: getEnvValue('AGORA_ACCOUNT', 'ds'),
-  password: getEnvValue('AGORA_PASSWORD', 'ssds'),
-  areaCode: parseInt(getEnvValue('AGORA_AREA_CODE', RtmAreaCode.glob.toString()), 10) || RtmAreaCode.glob,
-  encryptionMode: parseInt(getEnvValue('AGORA_ENCRYPTION_MODE', RtmEncryptionMode.none.toString()), 10) || RtmEncryptionMode.none,
-  encryptionKey: getEnvValue('AGORA_ENCRYPTION_KEY', ''),
+  token: getEnvValue('AGORA_TOKEN', 'token', ''),
+  channelName: getEnvValue('AGORA_CHANNEL_NAME', 'channelName', 'rtmtestrn'),
+  uid: getEnvValue('AGORA_UID', 'uid', ''),
+  logFilePath: getEnvValue('AGORA_LOG_FILE_PATH', 'logFilePath', ''),
+  server: getEnvValue('AGORA_SERVER', 'server', ''),
+  port: parseInt(getEnvValue('AGORA_PORT', 'port', '0'), 10) || 0,
+  proxyType: parseInt(getEnvValue('AGORA_PROXY_TYPE', 'proxyType', RtmProxyType.none.toString()), 10) || RtmProxyType.none,
+  account: getEnvValue('AGORA_ACCOUNT', 'account', 'ds'),
+  password: getEnvValue('AGORA_PASSWORD', 'password', 'ssds'),
+  areaCode: parseInt(getEnvValue('AGORA_AREA_CODE', 'areaCode', RtmAreaCode.glob.toString()), 10) || RtmAreaCode.glob,
+  encryptionMode: parseInt(getEnvValue('AGORA_ENCRYPTION_MODE', 'encryptionMode', RtmEncryptionMode.none.toString()), 10) || RtmEncryptionMode.none,
+  encryptionKey: getEnvValue('AGORA_ENCRYPTION_KEY', 'encryptionKey', ''),
   encryptionSalt: new Array(32).fill(1, 0, 32),
   // Token generation
-  tokenGenerationUrl: getEnvValue('AGORA_TOKEN_GEN_URL', ''),
-  tokenGenerationHeaders: getEnvValue('AGORA_TOKEN_GEN_HEADERS', '{}'),
-  readChannels: getEnvValue('AGORA_READ_CHANNELS', ''),
-  writeChannels: getEnvValue('AGORA_WRITE_CHANNELS', ''),
+  tokenGenerationUrl: getEnvValue('AGORA_TOKEN_GEN_URL', 'tokenGenerationUrl', ''),
+  tokenGenerationHeaders: getEnvValue('AGORA_TOKEN_GEN_HEADERS', 'tokenGenerationHeaders', '{}'),
+  readChannels: getEnvValue('AGORA_READ_CHANNELS', 'readChannels', ''),
+  writeChannels: getEnvValue('AGORA_WRITE_CHANNELS', 'writeChannels', ''),
 };
 
 export default config;
